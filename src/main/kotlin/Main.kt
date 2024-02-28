f3575b1f68db0b3c5463ed5bf9166c37303ff588
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2024-02-28 14:08:47 +0200

    remove comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,6 @@
 package org.accommodation.bookingservice
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
     println("Enter your city ...")
     val userCity = readlnOrNull();
 
